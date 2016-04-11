5d78f5e9419acdc3dd908d165b89669da3ee4f7c
sky <aslijiasheng@gmail.com>
2016-04-11 13:50:31 +0800

    modify router

--- baby/baby/routes/index.js
+++ baby/baby/routes/index.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET home page. test */
+// router.get('/home', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+// });
 
 /* Post home page. */
 router.post('/home', function(req, res, next) {
-    // console.log(req.body);
-    // console.log(req.body.data[0]);
-    res.render('index', { title: '宝贝助手' });
+    console.log(req.body);
+    var token = "";
+    if(!req.body.data.succ){
+        token = req.body.data.babyFormToken;
+    }
+    res.render('index', { message: '', token: token });
 });
 
 
 router.post('/home/reiver', function(req, res, next) {
-    var message = "";
-    if(!req.body.data.succ){
-        message = req.body.data.message;
+    var ret = req.body.data;
+    console.log(req.body.data);
+    if(!ret.succ){
+        message = ret.message;
     }
-    res.render('index', { message: message , identificationNumber: req.body.data.data.identificationNumber, email: req.body.data.data.email });
+    res.render('index', { message: message, token: ret.data.token.babyFormToken });
 })
 
 
