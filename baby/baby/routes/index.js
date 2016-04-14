b6425c2b82be2dc63934f8517eb11a061f4ec7ab
sky <aslijiasheng@gmail.com>
2016-04-14 20:35:08 +0800

    Duplicate Form Submission Token Key

--- baby/baby/routes/index.js
+++ baby/baby/routes/index.js
@@ -11,19 +11,22 @@ router.post('/home', function(req, res, next) {
     console.log(req.body);
     var token = "";
     if(!req.body.data.succ){
-        token = req.body.data.babyFormToken;
+        token = req.body.data.token;
+        secret = req.body.data.secret;
     }
-    res.render('index', { message: '', token: token });
+    res.render('index', { message: '', token: token, secret: secret });
 });
 
 
 router.post('/home/reiver', function(req, res, next) {
     var ret = req.body.data;
+    token = ret.data.token;
+    secret = ret.data.secret;
     console.log(req.body.data);
     if(!ret.succ){
         message = ret.message;
     }
-    res.render('index', { message: message, token: ret.data.token.babyFormToken });
+    res.render('index', { message: message, token: token, secret: secret });
 })
 
 
